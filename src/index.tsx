e7f29afbc6c3eb5b60bf7174b56cf96edfe53b73
uussong <77879633+uussong@users.noreply.github.com>
2023-12-24 23:23:05 +0900

    chore: React Router 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'create',
+        element: <div>create-form</div>,
+      },
+    ],
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
 
