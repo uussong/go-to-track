9259f4f3101e15c4d1589b884a32bc900ae65330
uussong <77879633+uussong@users.noreply.github.com>
2023-12-26 19:35:44 +0900

    feat: 기본 스타일 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Global } from '@emotion/react'
+import { globalStyles } from './styles/globalStyles'
 
 const queryClient = new QueryClient()
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
+        <Global styles={globalStyles} />
         <Outlet />
       </QueryClientProvider>
     </RecoilRoot>
