4e530f8529964ee3f5812f5d1dd7a8402148c892
uussong <77879633+uussong@users.noreply.github.com>
2023-12-31 22:24:51 +0900

    feat: getRedirectResult를 SignInPage로 이동

--- src/components/signin/GoogleSignIn.tsx
+++ src/components/signin/GoogleSignIn.tsx
@@ -1,14 +1,8 @@
-import { useEffect } from 'react'
-
-import { useNavigate } from 'react-router-dom'
 import { signInWithRedirect } from 'firebase/auth'
 
-import { getGoogleRedirectResult } from '@/remote/auth'
 import { auth, provider } from '@/remote/firebase'
 
 export default function GoogleSignIn() {
-  const navigate = useNavigate()
-
   const handleGoogleSignIn = async () => {
     try {
       await signInWithRedirect(auth, provider)
@@ -16,21 +10,6 @@ export default function GoogleSignIn() {
       alert('로그인을 다시 시도해 주세요.') // 변경 예정
     }
   }
-
-  useEffect(() => {
-    const getRedirectResult = async () => {
-      try {
-        const result = await getGoogleRedirectResult()
-        if (result) {
-          navigate('/myforms')
-        }
-      } catch (error) {
-        alert('로그인을 다시 시도해 주세요.') // 변경 예정
-      }
-    }
-
-    getRedirectResult()
-  }, [navigate])
 
   return (
     <>
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,33 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useUser } from '@/hooks/useUser'
+import { getGoogleRedirectResult } from '@/remote/auth'
+import Loading from '@/components/shared/Loading'
 import GoogleSignIn from '@/components/signin/GoogleSignIn'
 
 export default function SignInPage() {
+  const navigate = useNavigate()
+  const user = useUser()
+
+  useEffect(() => {
+    const getRedirectResult = async () => {
+      try {
+        const result = await getGoogleRedirectResult()
+
+        if (result) {
+          navigate('/myforms')
+        }
+      } catch (error) {
+        alert('로그인을 다시 시도해 주세요.') // 변경 예정
+      }
+    }
+    getRedirectResult()
+  }, [navigate])
+
+  if (user) {
+    return <Loading />
+  }
+
   return (
     <>
       <h1>지금 바로 시작해보세요</h1>
