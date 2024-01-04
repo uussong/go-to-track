392c0383e216964a8124aace10db415fa3190fd8
uussong <77879633+uussong@users.noreply.github.com>
2024-01-04 19:02:36 +0900

    design: GoogleSignIn에 google 로고 추가

--- src/components/signin/GoogleSignIn.tsx
+++ src/components/signin/GoogleSignIn.tsx
@@ -1,4 +1,5 @@
 import { signInWithRedirect } from 'firebase/auth'
+import { css } from '@emotion/react'
 import { auth, provider } from '@/remote/firebase'
 import { Button } from '../shared/button'
 
@@ -13,9 +14,35 @@ export default function GoogleSignIn() {
 
   return (
     <>
-      <Button variant={'secondary'} size={'fill'} onClick={handleGoogleSignIn}>
+      <Button
+        css={styles}
+        variant={'secondary'}
+        size={'fill'}
+        onClick={handleGoogleSignIn}
+      >
         Google로 시작하기
       </Button>
     </>
   )
 }
+
+const styles = css`
+  position: relative;
+
+  &:hover::before {
+    filter: grayscale(20%);
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 16px;
+    transform: translate(-50%, -50%);
+    width: 16px;
+    height: 16px;
+    background-image: url('/images/google.png');
+    background-size: cover;
+    background-position: center;
+  }
+`
