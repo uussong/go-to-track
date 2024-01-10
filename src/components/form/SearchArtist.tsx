d1a17c404bd86757e3b7f2118ec379eff955fcf0
uussong <77879633+uussong@users.noreply.github.com>
2024-01-10 11:50:23 +0900

    refactor: useSearchArtistInfo 훅 분리

--- src/components/form/SearchArtist.tsx
+++ src/components/form/SearchArtist.tsx
@@ -1,28 +1,16 @@
 import { useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { getArtistInfo } from '@/remote/spotify'
 import { useDebounce } from '@/hooks/useDebounce'
+import { useSearchArtistInfo } from '@/hooks/useSearchArtistInfo'
 import ArtistInfo from './ArtistInfo'
 
 export default function SearchArtist() {
   const [searchInput, setSearchInput] = useState('')
   const debouncedValue = useDebounce(searchInput)
+  const { data, isLoading } = useSearchArtistInfo(debouncedValue)
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value)
   }
-
-  const { data, isLoading } = useQuery({
-    queryKey: ['search', debouncedValue],
-    queryFn: () => getArtistInfo(debouncedValue),
-    enabled: debouncedValue !== '',
-    select: (data) =>
-      data.artists.items.filter((artist: any) =>
-        artist.genres.some(
-          (genre: any) => genre.includes('k-pop') || genre.includes('korean'),
-        ),
-      ),
-  })
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
