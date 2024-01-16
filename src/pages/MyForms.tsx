1488cdb5cefa61e07f00bfb8488d8f7f4ca8fac8
uussong <77879633+uussong@users.noreply.github.com>
2024-01-17 02:06:51 +0900

    feat: 설문지 생성 시 관련 정보 초기화

--- src/pages/MyForms.tsx
+++ src/pages/MyForms.tsx
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom'
+import { useSetRecoilState } from 'recoil'
+import { formDataState, formIdState } from '@/stores/form'
 import PageLayout from '@/components/shared/PageLayout'
 import { Button } from '@/components/shared/button'
 
 export default function MyFormsPage() {
+  const setFormId = useSetRecoilState(formIdState)
+  const setFormTitle = useSetRecoilState(formDataState)
+
+  const reserFormData = () => {
+    setFormId({ artistId: '', albumId: '' })
+    setFormTitle({ formTitle: '' })
+  }
+
   return (
     <PageLayout>
-      <Button>
-        <Link to={`/form`}>만들기</Link>
-      </Button>
+      <Link to={`/form`}>
+        <Button onClick={reserFormData}>만들기</Button>
+      </Link>
     </PageLayout>
   )
 }
