642a1bfa0a3d3b31d9210f503aa6e8fc981cd9a6
uussong <77879633+uussong@users.noreply.github.com>
2024-01-17 15:34:07 +0900

    feat: 설문 생성 시 formTitle 초기화

--- src/pages/MyForms.tsx
+++ src/pages/MyForms.tsx
@@ -1,11 +1,15 @@
+import { useSetRecoilState } from 'recoil'
 import { Link } from 'react-router-dom'
 import PageLayout from '@/components/shared/PageLayout'
 import { Button } from '@/components/shared/button'
+import { formDataState } from '@/stores/form'
 
 export default function MyFormsPage() {
+  const setFormTitle = useSetRecoilState(formDataState)
+
   return (
     <PageLayout>
-      <Link to={`/form`}>
+      <Link to={`/form`} onClick={() => setFormTitle({ formTitle: '' })}>
         <Button>만들기</Button>
       </Link>
     </PageLayout>
