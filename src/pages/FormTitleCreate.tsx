988511282921be36e04c0ffab6f65313ce05a87d
uussong <77879633+uussong@users.noreply.github.com>
2024-01-23 00:26:19 +0900

    feat: formTitleCreate에서 뒤로 가기 시 formTitle 초기화

--- src/pages/FormTitleCreate.tsx
+++ src/pages/FormTitleCreate.tsx
@@ -1,10 +1,28 @@
+import { useEffect } from 'react'
 import { useSetRecoilState } from 'recoil'
 import { formTitleState } from '@/stores/form'
 import PageLayout from '@/components/shared/PageLayout'
 import EnterTitle from '@/components/form/title/EnterTitle'
+import history from '@/routes/history'
 
 export default function FormTitleCreate() {
   const setFormTitle = useSetRecoilState(formTitleState)
+
+  useEffect(() => {
+    const listenBackEvent = () => {
+      setFormTitle('')
+    }
+
+    const historyEvent = history.listen(({ action, location }) => {
+      if (action === 'POP' && location.pathname === `/myforms`) {
+        listenBackEvent()
+      }
+    })
+
+    return () => {
+      historyEvent()
+    }
+  }, [setFormTitle])
 
   return (
     <PageLayout>
--- src/pages/MyForms.tsx
+++ src/pages/MyForms.tsx
@@ -1,15 +1,11 @@
-import { useSetRecoilState } from 'recoil'
 import { Link } from 'react-router-dom'
 import PageLayout from '@/components/shared/PageLayout'
 import { Button } from '@/components/shared/button'
-import { formTitleState } from '@/stores/form'
 
 export default function MyFormsPage() {
-  const setFormTitle = useSetRecoilState(formTitleState)
-
   return (
     <PageLayout>
-      <Link to={`/form/create/title`} onClick={() => setFormTitle('')}>
+      <Link to={`/form/create/title`}>
         <Button>만들기</Button>
       </Link>
     </PageLayout>
