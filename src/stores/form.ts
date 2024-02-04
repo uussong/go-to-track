cf531ec28662bfc7268d61f47dc9b58af6abf004
uussong <77879633+uussong@users.noreply.github.com>
2024-02-04 15:27:13 +0900

    feat: AtomEffect를 활용해 formTitle을 local storage에 저장

--- src/stores/form.ts
+++ src/stores/form.ts
@@ -1,6 +1,22 @@
-import { atom } from 'recoil'
+import { atom, AtomEffect } from 'recoil'
+
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key)
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue))
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue))
+    })
+  }
 
 export const formTitleState = atom({
   key: 'formTitleState',
   default: '',
+  effects: [localStorageEffect('formTitle')],
 })
