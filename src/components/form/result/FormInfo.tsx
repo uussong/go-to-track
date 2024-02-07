140189a2cc72b86b6b5988a1b924cc7e784cac44
uussong <77879633+uussong@users.noreply.github.com>
2024-02-08 00:04:26 +0900

    feat: artistId, albumId 활용해 필요한 데이터 가져오기

--- src/components/form/result/FormInfo.tsx
+++ src/components/form/result/FormInfo.tsx
@@ -1,4 +1,7 @@
 import { Button } from '@/components/shared/button'
+import { Text } from '@/components/shared/text'
+import { useGetAlbumInfo } from '@/hooks/useGetAlbumInfo'
+import { useGetArtistInfo } from '@/hooks/useGetArtistInfo'
 import { FormData } from '@/models/form'
 
 interface FormInfoProps {
@@ -7,10 +10,17 @@ interface FormInfoProps {
 }
 
 export default function FormInfo({ form, onNext }: FormInfoProps) {
+  const { data: artist } = useGetArtistInfo(form.artistId)
+  const { data: album } = useGetAlbumInfo(form.albumId)
+
   return (
-    <>
-      <div>FormInfo</div>
+    <section>
+      <div>
+        <Text variant={'heading3'}>{artist.name}</Text>
+        <Text variant={'heading2'}>{album.name}</Text>
+        <img src={album.images[1].url} alt={album.name} />
+      </div>
       <Button onClick={onNext}>투표하기</Button>
-    </>
+    </section>
   )
 }
--- src/components/form/result/SelectTrack.tsx
+++ src/components/form/result/SelectTrack.tsx
@@ -1,7 +1,38 @@
+import { useState } from 'react'
+import { Checkbox } from '@/components/shared/checkbox'
+import { useGetAlbumInfo } from '@/hooks/useGetAlbumInfo'
+import { TrackData } from '@/models/track'
+
 interface SelectTrackProps {
   albumId: string
 }
 
 export default function SelectTrack({ albumId }: SelectTrackProps) {
-  return <div>SelectTrack</div>
+  const { data: album } = useGetAlbumInfo(albumId)
+  const tracks = album.tracks.items
+
+  const [isChecked, setIsChecked] = useState<boolean[]>(
+    Array(tracks.length).fill(false),
+  )
+
+  const handleCheck = (checkedIndex: number, checked: boolean) => {
+    setIsChecked((prevData) =>
+      prevData.map((value, idx) =>
+        idx + 1 === checkedIndex ? checked : value,
+      ),
+    )
+  }
+
+  return (
+    <ul>
+      {tracks.map((track: TrackData, index: number) => (
+        <Checkbox
+          index={track.track_number}
+          label={track.name}
+          checked={isChecked[index]}
+          onClick={handleCheck}
+        />
+      ))}
+    </ul>
+  )
 }
