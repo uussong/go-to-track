f177f5ac33599b975cba1c3286f81867846abb01
uussong <77879633+uussong@users.noreply.github.com>
2024-02-16 22:36:06 +0900

    feat: 공유 링크 복사 시 메세지 표시 기능 추가

--- src/components/form/share/ShareLink.tsx
+++ src/components/form/share/ShareLink.tsx
@@ -1,21 +1,71 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Button } from '@/components/shared/button'
+import { Text } from '@/components/shared/text'
+import { css, keyframes } from '@emotion/react'
+import { colors } from '@/styles/colors'
 
 export default function ShareLink() {
+  const [stateType, setStateType] = useState('')
   const { formId } = useParams()
   const shareLink = `${window.location.origin}/${formId}`
 
   const copyLink = async () => {
     try {
       await navigator.clipboard.writeText(shareLink)
+      setStateType('success')
     } catch (error) {
       console.error(error)
+      setStateType('error')
+    } finally {
+      setTimeout(() => {
+        setStateType('')
+      }, 3000)
     }
   }
 
   return (
-    <Button variant={'secondary'} onClick={copyLink}>
-      공유
-    </Button>
+    <>
+      <Button variant={'secondary'} onClick={copyLink}>
+        공유
+      </Button>
+      {stateType === 'success' ? (
+        <div css={messageStyles(stateType)}>
+          <Text css={messageTextStyles}>링크가 복사되었어요!</Text>
+        </div>
+      ) : stateType === 'error' ? (
+        <div css={messageStyles(stateType)}>
+          <Text css={messageTextStyles}>다시 시도해 주세요.</Text>
+        </div>
+      ) : null}
+    </>
   )
 }
+
+const fadeInOut = keyframes`
+  0% {
+    opacity: 0;
+  }
+  25% {
+    opacity: 1;
+  }
+  75%{
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`
+
+const messageStyles = (stateType: string) => css`
+  position: absolute;
+  right: 16px;
+  background-color: ${stateType === 'success' ? colors.green : colors.red};
+  padding: 12px 16px;
+  border-radius: 10px;
+  animation: ${fadeInOut} 3s ease-in-out;
+`
+
+const messageTextStyles = css`
+  color: ${colors.white};
+`
