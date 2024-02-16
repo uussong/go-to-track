5f7de4f4e31bc063f442fcd248d53e96da6899eb
uussong <77879633+uussong@users.noreply.github.com>
2024-02-16 18:28:49 +0900

    feat: 링크 복사 기능 추가

--- src/components/form/share/ShareLink.tsx
+++ src/components/form/share/ShareLink.tsx
@@ -1,5 +1,21 @@
+import { useParams } from 'react-router-dom'
 import { Button } from '@/components/shared/button'
 
 export default function ShareLink() {
-  return <Button variant={'secondary'}>공유</Button>
+  const { formId } = useParams()
+  const shareLink = `${window.location.origin}/${formId}`
+
+  const copyLink = async () => {
+    try {
+      await navigator.clipboard.writeText(shareLink)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <Button variant={'secondary'} onClick={copyLink}>
+      공유
+    </Button>
+  )
 }
