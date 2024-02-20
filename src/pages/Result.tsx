7049a59e6cf4c176f38a4b5f44c46c2b8e512780
uussong <77879633+uussong@users.noreply.github.com>
2024-02-21 00:20:33 +0900

    feat: Navbar 추가

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,5 +1,26 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import PageLayout from '@/components/shared/PageLayout'
+import { useGetFormData } from '@/hooks/useGetFormData'
+import { FormDataFromUser } from '@/models/form'
+import useNavbar from '@/hooks/useNavbar'
+import Navbar from '@/components/form/result/Navbar'
+import SignOut from '@/components/shared/SignOut'
 
 export default function ResultPage() {
+  const { formId } = useParams()
+  const { data: form } = useGetFormData(formId ?? '') as {
+    data: FormDataFromUser
+  }
+
+  const { updateNavbar } = useNavbar()
+
+  useEffect(() => {
+    updateNavbar({
+      left: <Navbar formTitle={form.formTitle} />,
+      right: <SignOut />,
+    })
+  }, [updateNavbar, form.formTitle])
+
   return <PageLayout>ResultPage</PageLayout>
 }
