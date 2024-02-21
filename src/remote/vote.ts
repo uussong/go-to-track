e1297f6a4b7a4ca3deba3bb94c086d8bdc16add3
uussong <77879633+uussong@users.noreply.github.com>
2024-02-22 00:51:36 +0900

    feat: 특정 트랙별 투표 수를 세는 함수 추가

--- src/remote/vote.ts
+++ src/remote/vote.ts
@@ -14,3 +14,30 @@ export const getFormVoteCount = async (formId: string) => {
 
   return querySnapshot.size
 }
+
+export const getSelectedTrackCounts = async (formId: string) => {
+  const voteDataRef = collection(
+    store,
+    COLLECTIONS.VOTE,
+    formId,
+    COLLECTIONS.VOTEDATA,
+  )
+  const querySnapshot = await getDocs(voteDataRef)
+
+  const selectedTrackCounts: any = {}
+
+  querySnapshot.forEach((doc) => {
+    const voteData = doc.data()
+    const selectedTracks = voteData.vote.selectedTrack
+
+    selectedTracks.forEach((track: any) => {
+      if (selectedTrackCounts[track]) {
+        selectedTrackCounts[track]++
+      } else {
+        selectedTrackCounts[track] = 1
+      }
+    })
+  })
+
+  return selectedTrackCounts
+}
