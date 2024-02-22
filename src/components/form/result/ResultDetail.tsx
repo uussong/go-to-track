744ecb9b45745148c5a0819a76e6125967452aba
uussong <77879633+uussong@users.noreply.github.com>
2024-02-22 23:12:35 +0900

    feat: useGetTrackCounts 훅 생성 및 적용

--- src/components/form/result/ResultDetail.tsx
+++ src/components/form/result/ResultDetail.tsx
@@ -1,8 +1,14 @@
 import { Text } from '@/components/shared/text'
 import { SingleAlbumData } from '@/models/album'
+import { useParams } from 'react-router-dom'
+import { useGetTrackCounts } from '@/hooks/useGetTrackCounts'
 
 export default function ResultDetail({ album }: { album: SingleAlbumData }) {
+  const { formId } = useParams()
+  const { data: trackCounts } = useGetTrackCounts(formId!)
+
   const tracks = album.tracks.items
+
   return (
     <section>
       <Text variant={'heading2'}>{album.name}</Text>
@@ -10,6 +16,11 @@ export default function ResultDetail({ album }: { album: SingleAlbumData }) {
         {tracks.map((track) => (
           <li key={track.id}>
             <Text>{track.name}</Text>
+            <Text variant={'detailStrong'}>
+              {trackCounts[track.track_number]
+                ? trackCounts[track.track_number]
+                : 0}
+            </Text>
           </li>
         ))}
       </ul>
