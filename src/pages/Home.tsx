2050ca0c228b60e3916f96ee95514ef00324f874
uussong <77879633+uussong@users.noreply.github.com>
2024-03-05 03:13:56 +0900

    refactor: Navbar 업데이트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,17 +6,24 @@ import SignOut from '@/components/shared/SignOut'
 import { useGetAllFormData } from '@/hooks/useGetAllFormData'
 import AllFormList from '@/components/home/AllFormList'
 import Introduction from '@/components/home/Introduction'
+import { Text } from '@/components/shared/text'
+import { useUser } from '@/hooks/useUser'
 
 export default function HomePage() {
   const { updateNavbar } = useNavbar()
+  const user = useUser()
   const { data: formList } = useGetAllFormData()
 
   useEffect(() => {
     updateNavbar({
-      left: <Link to={`/`}>Home</Link>,
-      right: <SignOut />,
+      left: (
+        <Link to={`/`}>
+          <Text variant={'heading1'}>go-to-track</Text>
+        </Link>
+      ),
+      right: user && <SignOut />,
     })
-  }, [updateNavbar])
+  }, [updateNavbar, user])
 
   return (
     <PageLayout>
