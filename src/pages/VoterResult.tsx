4823b056432bc15f2926036b32d044014556e60f
uussong <77879633+uussong@users.noreply.github.com>
2024-03-04 12:56:13 +0900

    feat: VoterResult 페이지에 navbar 추가

--- src/pages/VoterResult.tsx
+++ src/pages/VoterResult.tsx
@@ -1,10 +1,13 @@
-import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import PageLayout from '@/components/shared/PageLayout'
 import { useGetVoteData } from '@/hooks/useGetVoteData'
 import { useGetAlbumInfo } from '@/hooks/useGetAlbumInfo'
 import { VoteData } from '@/models/vote'
 import ResultDetail from '@/components/form/result/voter/ResultDetail'
 import { useGetVoteCount } from '@/hooks/useGetVoteCount'
+import useNavbar from '@/hooks/useNavbar'
+import { Text } from '@/components/shared/text'
 
 export default function VoterResultPage() {
   const { userId, formId } = useParams()
@@ -14,6 +17,19 @@ export default function VoterResultPage() {
   }
   const { data: album } = useGetAlbumInfo(data.vote.albumId)
   const { data: voteCount } = useGetVoteCount(formId!)
+
+  const { updateNavbar } = useNavbar()
+
+  useEffect(() => {
+    updateNavbar({
+      left: (
+        <Link to={`/`}>
+          <Text variant={'heading1'}>go-to-track</Text>
+        </Link>
+      ),
+      right: null,
+    })
+  }, [updateNavbar])
 
   return (
     <PageLayout>
