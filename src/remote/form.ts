e9a3e67e230a0ec221108122c09e939ea5a67615
uussong <77879633+uussong@users.noreply.github.com>
2024-03-15 18:29:25 +0900

    refactor: 페이지네이션 구현을 위한 함수로 수정

--- src/remote/form.ts
+++ src/remote/form.ts
@@ -10,6 +10,8 @@ import {
   deleteDoc,
   setDoc,
   where,
+  startAfter,
+  limit,
 } from 'firebase/firestore'
 import { store } from './firebase'
 import {
@@ -34,12 +36,14 @@ export const saveFormData = async (user: User, formData: FormDataFromUser) => {
   await addDoc(formDataRef, formDataToSave)
 }
 
-export const getAllFormData = async () => {
-  const formRef = collection(store, COLLECTIONS.FORM)
-
-  const querySnapshot = await getDocs(formRef)
-
-  const formList: FormListData[] = querySnapshot.docs.map((doc) => {
+export const fetchInitialFormList = async () => {
+  const firstQuery = query(
+    collection(store, COLLECTIONS.FORM),
+    orderBy('timestamp'),
+    limit(2),
+  )
+  const firstSnapshot = await getDocs(firstQuery)
+  const formList: FormListData[] = firstSnapshot.docs.map((doc) => {
     const formData = doc.data()
     return {
       id: doc.id,
@@ -47,13 +51,41 @@ export const getAllFormData = async () => {
         albumId: formData.albumId,
         artistId: formData.artistId,
         formTitle: formData.formTitle,
-        uid: formData.uid,
         timestamp: formData.timestamp.toDate(),
       },
     }
   })
 
-  return formList
+  return {
+    formList,
+    lastVisible: firstSnapshot.docs[firstSnapshot.docs.length - 1],
+  }
+}
+
+export const fetchNextFormList = async (lastVisible: any) => {
+  const nextQuery = query(
+    collection(store, COLLECTIONS.FORM),
+    orderBy('timestamp'),
+    startAfter(lastVisible),
+    limit(2),
+  )
+  const nextSnapshot = await getDocs(nextQuery)
+  const formList: FormListData[] = nextSnapshot.docs.map((doc) => {
+    const formData = doc.data()
+    return {
+      id: doc.id,
+      data: {
+        albumId: formData.albumId,
+        artistId: formData.artistId,
+        formTitle: formData.formTitle,
+        timestamp: formData.timestamp.toDate(),
+      },
+    }
+  })
+  return {
+    formList,
+    lastVisible: nextSnapshot.docs[nextSnapshot.docs.length - 1],
+  }
 }
 
 export const getUserFormList = async (user: User) => {
