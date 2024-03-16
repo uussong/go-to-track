d2fd1d747d1f9df9c8d1027e0dce6790cb72c63a
uussong <77879633+uussong@users.noreply.github.com>
2024-03-16 17:26:29 +0900

    feat: 다음 페이지 불러오기 기능 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,11 +8,12 @@ import AllFormList from '@/components/home/AllFormList'
 import Introduction from '@/components/home/Introduction'
 import { Text } from '@/components/shared/text'
 import { useUser } from '@/hooks/useUser'
+import { Button } from '@/components/shared/button'
 
 export default function HomePage() {
   const { updateNavbar } = useNavbar()
   const user = useUser()
-  const { data } = usePaginatedFormData()
+  const { data, fetchNextPage } = usePaginatedFormData()
 
   const formList = data?.pages.flatMap((forms) => forms.formList)
 
@@ -31,6 +32,9 @@ export default function HomePage() {
     <PageLayout>
       <Introduction />
       {formList && <AllFormList formList={formList} />}
+      <Button variant={'secondary'} onClick={() => fetchNextPage()}>
+        불러오기
+      </Button>
     </PageLayout>
   )
 }
