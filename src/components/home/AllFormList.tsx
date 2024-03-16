0e5c6356c3d9d4bb354f98c06dbc71c9836d8eeb
uussong <77879633+uussong@users.noreply.github.com>
2024-03-16 20:12:05 +0900

    feat: FormItem 컴포넌트를 Suspense 사용해 비동기적으로 처리

--- src/components/home/AllFormList.tsx
+++ src/components/home/AllFormList.tsx
@@ -1,7 +1,9 @@
+import { Suspense } from 'react'
 import { css } from '@emotion/react'
 import { FormListData } from '@/models/form'
 import { Text } from '../shared/text'
 import FormItem from './FormItem'
+import { Skeleton } from '../shared/skeleton'
 
 interface AllFormListProps {
   formList: FormListData[]
@@ -12,7 +14,9 @@ export default function AllFormList({ formList }: AllFormListProps) {
     <section css={sectionStyles}>
       <Text variant={'heading2'}>지금 바로 최애곡을 뽑아보세요</Text>
       {formList.map((form) => (
-        <FormItem key={form.id} form={form} />
+        <Suspense fallback={<Skeleton height={162} margin={10} />}>
+          <FormItem key={form.id} form={form} />
+        </Suspense>
       ))}
     </section>
   )
