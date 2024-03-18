7c2afefe303f6bd4b4a55a755dd8b6d46697142f
uussong <77879633+uussong@users.noreply.github.com>
2024-03-18 14:56:30 +0900

    design: heading2에 margin 추가해 간격 조정

--- src/components/home/AllFormList.tsx
+++ src/components/home/AllFormList.tsx
@@ -16,7 +16,9 @@ export default function AllFormList({
 }: AllFormListProps) {
   return (
     <section css={sectionStyles}>
-      <Text variant={'heading2'}>지금 바로 최애곡을 뽑아보세요</Text>
+      <Text variant={'heading2'} css={headingStyles}>
+        지금 바로 최애곡을 뽑아보세요
+      </Text>
       {formList.map((form) => (
         <Suspense
           key={form.id}
@@ -33,4 +35,8 @@ export default function AllFormList({
 const sectionStyles = css`
   width: 100%;
   padding-top: 50px;
+`
+
+const headingStyles = css`
+  margin-bottom: 16px;
 `
